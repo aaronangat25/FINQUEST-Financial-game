d1f6d949a2c2c5db6fdbc372fcf4442a9aa99bc7
AaronAngat <aaronangat30@gmail.com>
2026-05-27 22:53:13 +0800

    created next and back button chapter selection

--- GD Scripts/Chapter Selection/chapter_selection.gd
+++ GD Scripts/Chapter Selection/chapter_selection.gd
@@ -2,7 +2,9 @@ extends Control
 
 const PROLOGUE_SCENE = "res://Scenes/Prologue/prologue.tscn"
 
-@onready var play_btn = $chapter_selection_bg/current_chapter/play_container/play_btn
+@onready var play_btn = $chapter_selection_bg/play_container/play_btn
+@onready var next_arrow_btn : Button = $chapter_selection_bg/next_chapter_btn
+@onready var back_arrow_btn : Button = $chapter_selection_bg/next_chapter_btn2
 
 func _ready():
 	play_btn.pressed.connect(_on_play_btn_pressed)
